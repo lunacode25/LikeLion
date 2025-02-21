25f24e540143b00fa56173eadd953e279c018067
lunacode25 <sysroute22@gmail.com>
2025-02-21 15:04:59 +0900

    loading

---
+++ loading/loading/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000); // 1초 대기
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000); // 1초 대기
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000); // 1초 대기
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000); // 1초 대기
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000); // 1초 대기
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000); // 1초 대기
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000); // 1초 대기
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000); // 1초 대기
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000); // 1초 대기
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000); // 1초 대기
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("■■■■■■■■■■");
+            Console.Clear(); // 화면 지우기
+            Console.WriteLine("텍스트엘든링");
+
+            Console.WriteLine("스토리1");
+
+        }
+    }
+}
