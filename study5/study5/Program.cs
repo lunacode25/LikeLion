2eed00f7525b87f17bb4637c5064529296c7bf8b
lunacode25 <sysroute22@gmail.com>
2025-02-21 14:35:09 +0900

    study5

---
+++ study5/study5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //실수형 데이터 형식: 소수점을 포함한 숫자를 표현하는 데이터 형식
+            //float singlePrecision = 3.14f; //f를 붙여서 float형으로 인식
+            //double doublePrecision = 3.1415926535; //기본적으로 double형으로 인식
+            //decimal highPrecision = 3.141592653589793238462643383279m; //m을 붙여서 decimal형으로 인식
+
+            //Console.WriteLine(singlePrecision);
+            //Console.WriteLine(doublePrecision);
+            //Console.WriteLine(highPrecision);   
+
+            //접미사 사용 : 숫자의 데이터형을 명시적으로 표현
+            //int interValue = 100; // 기본적으로 int형으로 인식
+            //long longValue = 100L; //L을 붙여서 long형으로 인식
+            //float floatValue = 3.14f; //f를 붙여서 float형으로 인식
+            //double doubleValue = 3.14; //기본적으로 double형으로 인식
+            //decimal decimalValue = 3.14m; //m을 붙여서 decimal형으로 인식
+
+            //Console.WriteLine(interValue); // 출력 : 100
+            //Console.WriteLine(longValue); // 출력 : 100
+            //Console.WriteLine(floatValue); // 출력 : 3.14
+            //Console.WriteLine(doubleValue); // 출력 : 3.14
+
+            //char형식: 단일 문자를 표현
+            //char letter = 'A'; // 문자형 데이터
+            //char symbol = '#'; // 특수 기호 저장
+            //char number = '9'; // 숫자 형태의 문자 저장
+
+            //Console.WriteLine(letter); // 출력 : A
+            //Console.WriteLine(symbol); // 출력 : #
+            //Console.WriteLine(number); // 출력 : 9
+
+            //string형식: 문자열을 표현
+            //string greeting = "Hello, World!"; // 문자열 데이터
+            //string name = "Alice"; // 문자열 데이터
+
+            //Console.WriteLine(greeting); // 출력 : Hello, World!
+            //Console.WriteLine(name); // 출력 : Alice
+
+            //bool형식: 참(true) = 1 , 거짓(false) = 0
+            // bool isRunning = true; // 참
+            //bool isFinished = false; // 거짓
+
+            //Console.WriteLine(isRunning); // 출력 : True
+            //Console.WriteLine(isFinished); // 출력 : False
+
+            //닷넷 형식 : 기본 형식의 닷넷 표현
+            Int32 number = 100; // int닷넷형식
+            System.String text = "Hello, World!"; // string형식
+
+            //Console.WriteLine(number); // 출력 : 100
+
+
+        }
+    }
+}
