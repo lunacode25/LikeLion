725b5e62da89c980b73e6b10c1828ffa0c525de5
lunacode25 <sysroute22@gmail.com>
2025-02-24 10:34:29 +0900

    TodayTask1

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //로딩바 시작화면
+            //게임스토리1
+            //비주얼노벨만들기
+            Console.ReadLine(); //엔터를 치면 다음으로 넘어가게 하는 코드
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■■");
+            
+            Console.WriteLine("로딩바 엔터를 치면 스토리가 시작됩니다. ");
+            Console.ReadLine(); //엔터를 치면 다음으로 넘어가게 하는 코드
+            Console.Clear(); //화면을 깨끗하게 지워주는 코드
+            Console.WriteLine("겨울이 오고, 전쟁이 시작된다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("오랜 평화는 이제 끝났고, 적들이 처들어온다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("겨울이 불러온 전쟁, 그 속에서 살아남을 자는 누구일까?");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("이제, 당신이 전장의 한복판에 서 있다. 전진하라.");
+
+
+
+
+        }
+    }
+}
