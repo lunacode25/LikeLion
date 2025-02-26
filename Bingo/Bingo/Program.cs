8f8dbaa8b90c24debec6205c56f8cb25303e6023
lunacode25 <sysroute22@gmail.com>
2025-02-26 14:04:26 +0900

    Bingo

--- Bingo/Bingo/Program.cs
+++ Bingo/Bingo/Program.cs
@@ -34,16 +34,147 @@ namespace Bingo
                 iArray[iB] = iT;
             }
 
+            int input = 0;
 
-            for (int i = 0; i < 5; i++)
+            int iCount = 0;
+            int iBingo = 0;
+            while (true)
             {
-                for (int j = 0; j < 5; j++)
-                {
-                    Console.Write(iArray[i * 5 + j].ToString("D2") + " ");
+                Console.Clear();
 
+                //빙고판
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+
+                        if (iArray[i * 5 + j] == 0)  //0일경우 체크
+                        {
+                            Console.Write(" * ");
+                        }
+                        else
+                        {
+                            Console.Write(iArray[i * 5 + j].ToString("D2") + " ");
+                        }
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
+                Console.WriteLine("빙고 숫자 : " + iBingo);
+                Console.WriteLine("숫자를 입력하세요: ");
+                input = int.Parse(Console.ReadLine());
+                iBingo = 0;
+                for (int i = 0; i < 25; i++)
+                {
+                    if (iArray[i] == input)
+                    {
+                        iArray[i] = 0;
+                        break;
+                    }
+                }
+
+
+                //빙고 체크 로직
+                //가로 체크
+                for (int i = 0; i < 5; ++i)
+                {
+                    for (int j = 0; j < 5; ++j)
+                    {
+                        if (iArray[i * 5 + j] == 0)
+                        {
+                            ++iCount;
+                        }
+
+                        if (iCount == 5)
+                        {
+                            ++iBingo;
+                        }
+                    }
+                    iCount = 0;
+                }
+
+                //세로 체크
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (iArray[i + 5 * j] == 0)
+                        {
+                            ++iCount;
+                        }
+
+                        if (iCount == 5)
+                        {
+                            ++iBingo;
+                        }
+                    }
+                    iCount = 0;
+                }
+
+
+
+                //대각선 오른쪽체크
+                //00 01 02 03 04
+                //05 06 07 08 09
+                //10 11 12 13 14
+                //15 16 17 18 19
+                //20 21 22 23 24
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if (iArray[i * 4 + 4] == 0)
+                    {
+                        ++iCount;
+                    }
+
+                    if (iCount == 5)
+                    {
+                        ++iBingo;
+                    }
+                }
+                iCount = 0;
+
+
+
+
+
+
+
+                //대각선 왼쪽체크
+                //00 01 02 03 04
+                //05 06 07 08 09
+                //10 11 12 13 14
+                //15 16 17 18 19
+                //20 21 22 23 24
+                for (int i = 0; i < 5; i++)
+                {
+                    if (iArray[i * 6] == 0)
+                    {
+                        ++iCount;
+                    }
+
+                    if (iCount == 5)
+                    {
+                        ++iBingo;
+                    }
+                }
+                iCount = 0;
+
+
+                if (iBingo >= 5)
+                {
+                    Console.WriteLine("빙고 성공");
+                    break;
+                }
             }
+
+
+
+
+
+
+
+
+
 
 
 
